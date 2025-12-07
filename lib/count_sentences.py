8ce03a606cd1df8a03eb0f3b77d4fd7ce168f8de
Ian <ian.nasoore@student.moringaschool.com>
2025-12-07 12:52:22 +0300

    complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+  def __init__(self, value=""):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      self._value = ""
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, new_value):
+    if not isinstance(new_value, str):
+      print("The value must be a string.")
+    else:
+      self._value = new_value
+
+  def is_sentence(self):
+    return isinstance(self._value, str) and self._value.endswith('.')
+
+  def is_question(self):
+    return isinstance(self._value, str) and self._value.endswith('?')
+
+  def is_exclamation(self):
+    return isinstance(self._value, str) and self._value.endswith('!')
+
+  def count_sentences(self):
+    if not isinstance(self._value, str) or self._value == "":
+      return 0
+    # Count groups of sentence-ending punctuation (., ?, !) as sentences
+    matches = re.findall(r'[.?!]+', self._value)
+    return len(matches)
